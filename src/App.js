2b9318f557c987206b51670587f4dd5a06a37cab
bmwasonga <bmwasonga@gmail.com>
2021-07-07 00:35:53 +0300

    has refused to work

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import ReactMarkdown from 'react-markdown';
 
-function App() {
+export default function App() {
+  const [markDown, useMarkdown] = useState('');
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <section className="markdown">
+        <textarea
+          className="input"
+          value={markDown}
+          onChange={(e) => useMarkdown(e.target.value)}
+        ></textarea>
+        <article className="result">
+          <ReactMarkdown>{markDown}</ReactMarkdown>
+        </article>
+      </section>
+    </main>
   );
 }
-
-export default App;
